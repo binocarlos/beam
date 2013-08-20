6f86d57ce4f7f9b7bb4732a7c61bb8a5ec0fa7a1
Solomon Hykes <solomon@dotcloud.com>
2013-08-20 00:01:12 -0700

    The command-line tool can connect to a remote redis and listen for jobs

--- cmd/beam/beam.go
+++ cmd/beam/beam.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"log"
 	"fmt"
+	"flag"
+	"github.com/dotcloud/beam"
 )
 
 // This is a placeholder for the beam command-line tool.
 // It is meant as a convenience to expose capabilities of the protocol and library
 // from the command-line.
 func main() {
-	fmt.Printf("Usage: beam")
+	flag.Parse()
+	worker := beam.NewWorker(&beam.NetTransport{"tcp", flag.Arg(0)}, "/jobs")
+	worker.RegisterJob("hello", JobHello)
+	if err := worker.Work(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func JobHello(name string, args []string, env map[string]string, streams *beam.Streamer, db beam.DB) error {
+	fmt.Printf("Hello world!\n")
+	return nil
 }
